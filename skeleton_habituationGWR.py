31bd6dbb8afb04ce226f0a76619c356a3de312ea
Jenny Zeng <jennyccinno@gmail.com>
2020-07-16 13:38:57 -0500

    Finished all to-do items, code runs and plots the model.

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -16,16 +16,19 @@ import matplotlib.pyplot as plt
 ###############################
 # TODO: Set a variable called stv to 4, this will define 
 #        the weight of the connection from input to output
-# stv = 
+stv = 4
 
 dec=0.7 # set weight decrement for habituation
 
 pls=[0, 0, 1, 0, 0] # set up a pulse
 
 # TODO: then create a list of 6 pulses, called x, to use for input
-# x = 
+x = pls*6 + [0] * 10 + pls * 6
 
 v = stv # Set connection weight to start weight value
+
+forgetflag = True # allows the model to simulate the process of forgetting the new association it learned
+# if the pulse is not felt for awhile, forget that it has recently been safe to feel the pulse and to not react
 
 ###############################
 # Set up and run simulation
@@ -34,19 +37,26 @@ v = stv # Set connection weight to start weight value
 # TODO: Find the length of the input list (x)
 #        and assign its value to a new variable, nTs
 #        (which tracks the number of Time Steps)
-# nTs = 
+nTs = len(x)
 
 # TODO: Create a numpy array named y, a 1D vector
 #        (row vector) padded with zeros, with one
 #        element per each time step
-# y = 
+y = np.zeros((1,nTs))
 
 # TODO: use a for-loop to iterate 
 #        through each time step in 
 #        the input series and calculate
 #        the output at each time step. Ex:
-# for ...
-
+for timeStep in range(nTs):
+    y[0,timeStep] = v * x[timeStep]
+    
+    if x[timeStep] > 0: # means that stimulation occured
+        v *= dec # if there's stimulation, we need to reduce the weight of the connection
+    
+    if timeStep > 3 and sum(x[timeStep-4:timeStep])==0 and v<stv and forgetflag:
+        v += (stv-v) * .05 # forget the associatation made if it doesn't occur for a while
+        
 #     then indent 4 spaces and write the equation that
 #     describes how each input value in the vector x is 
 #     transformed to the output value in the vector y
